0a2c6808a79452101512c0743219ca316cffcc52
chicha11 <75187259+chicha11@users.noreply.github.com>
2023-10-24 23:53:19 +0530

    changes in 404 page styling

--- src/Error.js
+++ src/Error.js
@@ -5,7 +5,7 @@ const Error = () => {
   return (
     <>
       <div className="gif-class">
-        <img src="../404_animation.gif" alt="404Error"></img>
+        <img className="error-img" src="../404_animation.gif" alt="404Error"></img>
       </div>
       <div className="text-class">
       <span class="text404">OOPS! THE PAGE YOU HAVE BEEN LOOKING FOR DOESNOT EXIST</span><br/>
